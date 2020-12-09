531540447c09d7dadcb5188fc4e1104417e7bce5
artem <abrazhnikov@aitheon.com>
2020-12-09 09:26:00 +0200

    add fund metamask script

---
+++ fund-metamask.js
@@ -0,0 +1,12 @@
+const receiver = "0xE425355F0A7FA15bE690F8DFCfD32B1EfA3bAAF9";
+const amount = web3.utils.toWei("10", 'ether');
+
+module.exports = async function(callback) {
+  const addresses = await web3.eth.getAccounts()
+
+  web3.eth.sendTransaction({
+    from: addresses[1],
+    to: receiver, 
+    value: amount
+  }, callback)
+}
