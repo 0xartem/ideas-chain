03658019206bf486e1a820b023191c24e66ef588
artem <abrazhnikov@aitheon.com>
2021-01-13 11:04:16 +0200

    add buy tokens logic

--- contracts/token/IdeasChainICO.sol
+++ contracts/token/IdeasChainICO.sol
@@ -2,12 +2,24 @@
 pragma solidity >=0.7.0 <0.8.0;
 
 import "./IdeasChainToken.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 
 contract IdeasChainICO {
+  using SafeMath for uint256;
 
   IdeasChainToken token;
+  uint256 public RATE = 1000; // 1 ETH = 1000 ICT
 
   constructor(address _tokenAddr) {
     token = IdeasChainToken(_tokenAddr);
+  }
+
+  fallback() external payable {
+    uint256 _tokenAmount = _getTokenAmount(msg.value);
+    token.transfer(msg.sender, _tokenAmount);
+  }
+
+  function _getTokenAmount(uint256 _weiAmount) internal view returns (uint256) {
+    return _weiAmount.div(10 ** 18).mul(RATE);
   }
 }
--- test/integration/token.js
+++ test/integration/token.js
@@ -1,6 +1,12 @@
 const IdeasChainToken = artifacts.require("IdeasChainToken")
 const IdeasChainICO = artifacts.require("IdeasChainICO")
 
+const web3 = require("web3")
+
+const { 
+  utils: { toWei },
+} = web3
+
 contract("token", (accounts) => {
 
   it("distributes token supply", async () => {
@@ -12,5 +18,26 @@ contract("token", (accounts) => {
 
     assert.equal(icoBalance.toString(), "750000")
     assert.equal(founderBalance.toString(), "250000")
+  })
+
+  it("can buy tokens", async () => {
+    const token = await IdeasChainToken.deployed()
+    const ico = await IdeasChainICO.deployed()
+
+    const userAddr = accounts[1] // A random account that we control
+    const wei = toWei("1", "ether") // We need to specify the value in wei
+
+    await ico.sendTransaction({
+      from: userAddr,
+      value: wei,
+    })
+
+    const userBalance = await token.balanceOf.call(userAddr);
+
+    assert.equal(userBalance.toString(), "1000")
+
+    const icoBalance = await token.balanceOf.call(ico.address);
+
+    assert.equal(icoBalance.toString(), "749000")
   })
 })
